13bb7cb9cffeee9ebb4a4cc4464d9c6071e99552
kaze113 <nthuyanh.hut@gmail.com>
2015-01-28 14:02:32 +0700

    Finish creating user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+
+  get 'users/edit'
+
+  get 'users/index'
+
+  get 'users/show'
+
   root                'static_pages#home'
   get    'help'    => 'static_pages#help'
   get    'about'   => 'static_pages#about'
   get    'contact' => 'static_pages#contact'
+  get    'signup'	 => 'users#new'
 
 end
