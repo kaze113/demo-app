e1d8b911176582fd556526a0f20f84f8cf74ae34
thuyanh <nguyen.thuy.anh@framgia.com>
2015-01-28 10:27:43 +0700

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
- 
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-
   root 'application#hello'
 
+#  get      home   => 'static_pages#home'
+#  get      help   => 'static_pages#help'
+#  get      about  => 'static_pages#about'
+
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
 
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,24 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Demo App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Demo App"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Demo App"
   end
 
 end
+
